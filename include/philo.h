4736378ca7c2c170b64245e63bdc19e0203e540a
minji-dan <aksen569@naver.com>
2022-04-29 19:05:18 +0900

    style

---
+++ include/philo.h
@@ -0,0 +1,40 @@
+#ifndef PHILO_H
+#define PHILO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/time.h>
+
+typedef struct s_philo
+{
+    int my_seat;
+    int l_fork;
+    int r_fork;
+    int eat_time;
+    long long check_last_eat_time;
+
+    t_info *info;
+} t_philo;
+
+typedef struct s_info
+{
+    pthread_mutex_t *fork_mu;
+
+    int num_of_philo;
+    int time_to_die;
+    int time_to_eat;
+    int time_to_sleep;
+    int num_must_eat;
+
+    long long curr_time;
+    long long start_time;
+
+    t_philo *philos;
+
+    pthread_mutex_t print;
+    pthread_mutex_t eating;
+} t_info;
+
+#endif
