105a49b8fe0da76845b2092d53b841d2b323c73b
minji-dan <aksen569@naver.com>
2022-05-01 12:30:32 +0900

    philo구조체 초기화

--- srcs/init.c
+++ srcs/init.c
@@ -6,6 +6,21 @@ static int init_mut(t_info *info)
 
 static int init_philo(t_dining *dining)
 {
+    int i;
+
+    dining->philos = malloc(sizeof(t_philo) * (dining->info.num_of_philo));
+    if (!dining->philos)
+        return (0);
+    while (++i < dining->info.num_of_philo)
+    {
+        dining->philos[i].my_seat = i;
+        dining->philos[i].l_fork = i;
+        dining->philos[i].r_fork = (i + 1) % dining->info.num_of_philo;
+        dining->philos[i].eat_time = 0;
+        dining->philos[i].info = &dining->info;
+        dining->philos[i].check_last_eat_time = 0;
+    }
+    return (1);
 }
 
 int init(t_dining *dining)
