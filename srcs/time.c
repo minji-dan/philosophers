1771ea8adb1ba4ef81d14d6eb9488955d635ad6c
minji-dan <aksen569@naver.com>
2022-05-03 00:46:04 +0900

    time 함수

--- srcs/time.c
+++ srcs/time.c
@@ -2,8 +2,33 @@
 
 long long get_time(void)
 {
+    struct timeval time;
+    long long ret;
+
+    gettimeofday(&time, NULL);
+    ret = time.tv_sec * 1000 + time.tv_usec / 1000;
+    return (ret);
 }
 
 void check_time(t_info *info, int mode)
 {
+    long long start;
+    long long curr;
+    long long dur;
+    long long tmp;
+
+    if (mode == 1)
+        dur = info->time_to_eat;
+    else if (mode == 2)
+        dur = info->time_to_sleep;
+    tmp = dur * 1000 / 2;
+    start = ft_get_time();
+    while (!(info->finish))
+    {
+        curr = ft_get_time();
+        if (dur <= (curr - start) || tmp == 1)
+            return;
+        usleep(tmp);
+        tmp /= 2;
+    }
 }
